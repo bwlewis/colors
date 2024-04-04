f610a5f2cebb46686e71a77440f591e09abd828b
Bryan W Lewis <blewis@illposed.net>
2024-04-03 22:05:10 -0400

    Examine reflectance curves...

---
+++ refl_crayola_blue.m
@@ -0,0 +1,20 @@
+% This script assumes you've run the 'getdata' script to set up all the data matrices:
+% the ``reflectance'' curve matrix R, wavelength vector w.
+
+% Even though the paint colors were measured under different light sources, their
+% computed reflectance curves should be approximately the same (we hope).
+
+% Let's investigate that...
+
+% The 'crayola-blue' colors for each lamp from the 'measurement' vector are in
+% columns:
+%  5  GrowLight-crayola-blue
+% 18  incandescent-crayola-blue
+% 30  LED-crayola-blue
+% 43  xenon-cray-blue
+% 44  xenon-cray-blue2
+
+plot(w, R(:,5), w, R(:,18), w, R(:,30), w, R(:,43), w, R(:,44))
+legend(measurement{5}, measurement{18}, measurement{30}, measurement{43}, measurement{44}, 'Location', 'northwest', 'FontSize', 24)
+
+%% These are all pretty close **except** for the xenon-cray-blue2, which looks wrong. So let's not use that color.
---
+++ refl_crayola_yellow.m
@@ -0,0 +1,19 @@
+% This script assumes you've run the 'getdata' script to set up all the data matrices:
+% the ``reflectance'' curve matrix R, wavelength vector w.
+
+% Even though the paint colors were measured under different light sources, their
+% computed reflectance curves should be approximately the same (we hope).
+
+% Let's investigate that...
+
+% The 'crayola-blue' colors for each lamp from the 'measurement' vector are in
+% columns:
+%  7 GrowLight
+% 20 incandescent
+% 31  LED
+% 46 xenon
+
+plot(w, R(:,7), w, R(:,20), w, R(:,31), w, R(:,46))
+legend(measurement{7}, measurement{20}, measurement{31},measurement{46}, 'Location', 'northwest', 'FontSize', 24)
+
+%% All look pretty good!
