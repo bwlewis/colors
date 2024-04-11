8100eb04e687a67764ddb56d8d5c991219c792f3
Bryan W Lewis <blewis@illposed.net>
2024-04-11 08:02:41 -0400

    Switched lscolor to use indices

--- lscolor.m
+++ lscolor.m
@@ -3,33 +3,29 @@
 %% This code assumes getdata was run.
 
 %% This is a function that takes the following arguments
-%% r  -- the reflectance curve to match
-%% P  -- a matrix of pigment reflectance curves to use
+%% r  -- the  index of the reflectance curve to match
+%% p  -- column indices of pigment reflectance curves to use
+%% R  -- the matrix  of spectral reflectance curves
 %% w  -- vector of wavelengths
 %% n  -- vector of pigment names
 
 %% It plots the original curve and estimated mixture,
 %% and returns a vector of pigment concentrations.
 
-%% the function call is something like:
-
-% getdata2
-% lscolor(R(:,1:11), R(:,12) w, measurement);   %% match 'green' color using all paints
-
-function x = lscolor(r, P, w, n)
+function x = lscolor(r, p, R, w, n)
 
   %% wavelength range ... limit result to visible spectrum between 400 and 700 nanometers:
   k = (400 <= w & w <= 700);
 
-  A = f(P(k,:));
-  x = lsqnonneg(A, f(r(k,:)));
-  plot(w(k,:), finv(f(r(k,:))), w(k,:), finv(A * x));
+  A = f(R(k,p));
+  x = lsqnonneg(A, f(R(k,r)));
+  plot(w(k,:), finv(f(R(k,r))), w(k,:), finv(A * x));
   legend("Target color", "Estimated match");
   x = x / sum(x);       %% re-scale into per-cent
 
   printf("Percent\tPigment\n");
-  for i = 1:length(n)
-    printf("%02.1f\t%s\n", 100*x(i), n(i){1});
+  for i = 1:length(p)
+    printf("%02.1f\t%s\n", 100*x(i), n(p(i)){1});
   end
 
 endfunction
